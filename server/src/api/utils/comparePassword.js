0692ec9e461f9db671d91cfd81c25b8ea45f0c11
dangquochuy <quochuy22122002@gmail.com>
2023-11-07 14:56:27 +0700

    git remove server

--- server/src/api/utils/comparePassword.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const ComparePassword = async (password, hashedPassword) => {
-    return bcrypt.compare(password, hashedPassword);
-};
-
-module.exports = ComparePassword;
